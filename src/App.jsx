7e8b73cdc5ec6dec7b0b622c6bc5b80daca59cfa
Adriano <adrianoact@msn.com>
2024-08-06 21:23:59 -0300

    Getting started with React - Properties

--- src/App.jsx
+++ src/App.jsx
@@ -4,12 +4,14 @@ export function App() {
 
   return (
     <div>
-      <Post />
-      <Post />
-      <Post />
-      <Post />
-      <Post />
-      <Post />
+      <Post
+        author="Adriano Tobias"
+        content="Lorem, ipsum dolor sit amet consectetur adipisicing elit. Maxime itaque quas corporis beatae veritatis, reprehenderit asperiores vitae quod possimus qui dignissimos unde deleniti consequatur quae, repellat debitis sunt, est rerum!"
+      />
+      <Post
+        author="Goku"
+        content="Um novo post muito legal"
+      />
     </div>
   )
 }
